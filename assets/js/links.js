1f89bb8004018ce1ee5b3c7d632e8141e072e0d5
Hooman <68550937+AlexBuscher09@users.noreply.github.com>
2020-10-20 20:20:26 +0300

    Update links.js

--- assets/js/links.js
+++ assets/js/links.js
@@ -4,9 +4,9 @@
 window.onload = function () {
     var currentURL = window.location.href;
 
-    if (currentURL != "https://alexbuscher09.github.io/ovi-demo/") {
-      document.getElementById("homeLink").href = "https://alexbuscher09.github.io/ovi-demo/";
+    if (currentURL != "/") {
+      document.getElementById("homeLink").href = "/";
       document.getElementById("serviceLink").href = "#top"; //change this
-      document.getElementById("contactLink").href = "https://alexbuscher09.github.io/ovi-demo/contacts.html";
+      document.getElementById("contactLink").href = "contacts.html";
     }
   }
